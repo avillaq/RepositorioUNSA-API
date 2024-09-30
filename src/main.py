380be912fb7c16a09a9204543243682c35515089
avillaq <villafuertequispealex@gmail.com>
2024-09-30 18:56:12 -0500

    Coneccion a la base de datos

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route("/")
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://root:@localhost:3306/repositorio_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+@app.route("/api/docs")
 def hello_world():
     return "<p>Hello, World!</p>"
