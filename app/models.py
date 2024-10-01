4f1cb25f9e9c69e47f55f1e55b5d88fba22ecd3c
avillaq <villafuertequispealex@gmail.com>
2024-10-01 11:11:21 -0500

    Referencia a la tabla documentos en el modelo Documento

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from . import db
 
-class Documentos(db.Model):
+class Documento(db.Model):
+    __tablename__ = 'documentos'
+
     id_documento = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.String(255), nullable=False)
     resumen = db.Column(db.Text, nullable=True)
@@ -9,6 +11,9 @@ class Documentos(db.Model):
     url_pdf = db.Column(db.String(255), nullable=True)
     id_coleccion = db.Column(db.Integer, nullable=True)
     id_editor = db.Column(db.Integer, nullable=True)
+
+    def __repr__(self):
+        return f"<Documento {self.titulo}>"
 
     def format(self):
         return {
