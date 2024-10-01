d243fa9aee12c9bd273cb7b381cb401db0278b87
avillaq <villafuertequispealex@gmail.com>
2024-10-01 17:59:30 -0500

    Se añade un filtro basico.

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,10 +1,25 @@
-from flask import jsonify
+from flask import jsonify, request
 from app.api.models import Documento
 from app.api import bp
 
 @bp.route('/documentos', methods=['GET'])
 def get_documentos():
-    documentos = Documento.query.all()
+    titulo = request.args.get('titulo')
+    fecha = request.args.get('fecha')
+    uri = request.args.get('uri')
+
+    query = Documento.query
+
+    # Filtrar la consulta
+    if titulo:
+        query = query.filter(Documento.titulo.like(f'%{titulo}%'))
+    if fecha:
+        query = query.filter_by(fecha=fecha)
+    if uri:
+        query = query.filter_by(uri=uri)
+
+    documentos = query.all()
+
     resultado = [documento.format() for documento in documentos]
     return jsonify(resultado)
 
