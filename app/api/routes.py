0498859754aa809bde96f5818134f8f0de643858
avillaq <villafuertequispealex@gmail.com>
2024-10-01 18:07:08 -0500

    Se añade paramentros para ordenar los resultados

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,12 +1,17 @@
 from flask import jsonify, request
 from app.api.models import Documento
 from app.api import bp
+from app.extensions import db
 
 @bp.route('/documentos', methods=['GET'])
 def get_documentos():
     titulo = request.args.get('titulo')
     fecha = request.args.get('fecha')
     uri = request.args.get('uri')
+
+    # Parametros para ordenar
+    sort_by = request.args.get('sort_by', 'titulo')  # Por defecto es el título
+    order = request.args.get('order', 'asc')  # Por defecto en orden ascendente
 
     query = Documento.query
 
@@ -18,6 +23,14 @@ def get_documentos():
     if uri:
         query = query.filter_by(uri=uri)
 
+    # Ordenar la consulta
+    if sort_by in ['titulo', 'fecha', 'uri']:
+        if order == 'desc':
+            query = query.order_by(db.desc(getattr(Documento, sort_by)))
+        else:
+            query = query.order_by(getattr(Documento, sort_by))
+
+    # Consultamos
     documentos = query.all()
 
     resultado = [documento.format() for documento in documentos]
