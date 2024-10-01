48823163c9c3d0c135d9352ee86fe1816f9f053e
avillaq <villafuertequispealex@gmail.com>
2024-10-01 15:23:30 -0500

    Prefijo api añadido

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,12 @@ from app.extensions import db
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('app.config.Config')
+    app.config.from_object('config.Config')
 
     db.init_app(app)
 
     # Registrar los Blueprints
     from app.api import bp as api_bp
-    app.register_blueprint(api_bp)
+    app.register_blueprint(api_bp, url_prefix='/api')
 
     return app
