97da841b574a08639640c4a0f174c92a07400d62
avillaq <villafuertequispealex@gmail.com>
2024-10-13 15:47:23 -0500

    Añadimos la documentacion automatica con swagger

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app.extensions import db, limiter, cache
+from app.swagger import swaggerui_blueprint, SWAGGER_URL
 
 def create_app():
     app = Flask(__name__)
@@ -12,5 +13,8 @@ def create_app():
     # Registrar los Blueprints
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
+
+    # Registrar Swagger UI
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
     return app
