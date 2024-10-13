052814c6c6f68b1bb87c509f76cd010c07b4777f
avillaq <villafuertequispealex@gmail.com>
2024-10-13 13:53:50 -0500

    Nueva extension

--- app/extensions.py
+++ app/extensions.py
@@ -1,2 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+# Conexión a la base de datos
 db = SQLAlchemy()
+
+# Configuracion para limitar el número de peticiones
+limiter = Limiter(
+    get_remote_address,
+    default_limits=["200/day", "50/hour"],
+)
