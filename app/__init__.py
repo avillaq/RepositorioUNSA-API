8457cf3433c21cddc425508c017f3e1de85ac706
avillaq <villafuertequispealex@gmail.com>
2024-10-13 13:54:41 -0500

    Uso de la app principal para iniciar limiter

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from app.extensions import db
+from app.extensions import db, limiter
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
 
     db.init_app(app)
+    limiter.init_app(app)
 
     # Registrar los Blueprints
     from app.api import bp as api_bp
